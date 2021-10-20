1609d4fecdf0cfbfaf7471834997e5e6dcf1ed2e
nachen1991 <na.chen@viewnext.com>
2021-10-20 18:19:36 +0200

    BD repository

--- microservicios/demo/src/main/java/com/example/DemoApplication.java
+++ microservicios/demo/src/main/java/com/example/DemoApplication.java
@@ -1,8 +1,13 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.infrastructure.repositories.ActorRepository;
+import com.example.ioc.ServicioMockImpl;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
@@ -10,10 +15,18 @@ public class DemoApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+	@Autowired
+	ServicioMockImpl srv;
+	@Value("${mi.propia.clave}")
+	String name;
+	@Autowired
+	ActorRepository dao;
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		System.out.println("Soy" + name);
+		
+		dao.findAll().forEach(System.out::println);
 		
 	}
 
